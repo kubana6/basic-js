bd1e504d6c2706777e513bb74e7026a4abf0fb97
Anton <kubana6@gmail.com>
2020-10-31 22:29:00 +0300

    make extended task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater( str, options) {
+    let repeatString =""
+    if(!options.repeatTimes && !options.additionRepeatTimes) {
+      return str + options.addition
+    }
+    for (let i = 0; i < options.repeatTimes; i +=1) {
+      repeatString += str
+      let a = 0
+      while(a < options.additionRepeatTimes ) {
+        repeatString += options.addition
+        if(a+1 != options.additionRepeatTimes) {
+          repeatString += options.additionSeparator ||"|"
+        }
+        a +=1
+      }
+      if(i+1 != options.repeatTimes) {
+        repeatString += options.separator || "+"
+      }
+    }
+   return repeatString
 };
-  
--- src/what-season.js
+++ src/what-season.js
@@ -4,28 +4,29 @@ module.exports = function getSeason(date = 0) {
   if(date === 0 ) {
     return 'Unable to determine the time of year!'
   }
-  const Month=date.getMonth()
+  const Month = date.getMonth()
+
   if(typeof date != 'object' ) {
-    throw new CustomError('THROWN')
+    throw new CustomError('null')
   }
   switch(Month) {
-    case 11: 
+    case 11:
     case 0:
     case 1:
      return 'winter'
-    case 2: 
+    case 2:
     case 3:
     case 4:
      return 'spring'
-    case 5: 
+    case 5:
     case 6:
     case 7:
      return 'summer'
-    case 8: 
+    case 8:
     case 9:
     case 10:
      return 'autumn'
     default:
-      throw new CustomError('THROWN')
+      throw new CustomError('Error')
   }
 };
