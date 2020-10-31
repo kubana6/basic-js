5234ebee85dc360ff3db77edf62dca9a6986c651
Anton <kubana6@gmail.com>
2020-11-01 00:09:34 +0300

    finish: basic js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,41 +1,56 @@
 const CustomError = require("../extensions/custom-error");
-const deletNextElement ='--discard-next'
-const deletPrevElement ='--discard-prev'
-const doubleNextElement ='--double-next'
-const doublePrevElement ='`--double-prev'
+
 
 module.exports = function transform(arr) {
-  if (Array.isArray(arr)) {
-    applyDouble(arr);
-    applyDiscard(arr);
-    return arr;
-  } else {
-    throw new Error();
-  }
-};
+  if (!Array.isArray(arr)) throw new Error();
 
-let applyDouble = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    switch (array[i]) {
+  let resultArr = [];
+  const arrLength = arr.length;
+
+  const discardNext = (i) => {
+    i += 1;
+
+    return (arr[i + 1] === '--double-prev' || arr[i + 1] === '--discard-prev')
+      ? i + 1
+      : i;
+  };
+
+  const doubleNext = (i) => {
+    if ((i + 1) < arrLength) {
+      resultArr.push(arr[i + 1]);
+    }
+  };
+
+  const doublePrev = (i) => {
+    if (i - 1 >= 0) {
+      resultArr.push(arr[i - 1]);
+    }
+  };
+
+  const discardPrev = () => {
+    resultArr.pop();
+  };
+
+  for (let i = 0; i < arrLength; i++) {
+    const element = arr[i];
+
+    switch (element) {
+      case '--discard-next':
+        i = discardNext(i);
+        break;
       case '--double-next':
-        if (array[i + 1] === undefined) { array.splice(i, 1); i -= 1; } else { array.splice(i, 1, array[i + 1]); i += 1; }
+        doubleNext(i);
         break;
       case '--double-prev':
-        if (array[i - 1] === undefined) { array.splice(i, 1); i -= 1; } else { array.splice(i, 1, array[i - 1]) }
-        break;
-    }
-  }
-}
-
-let applyDiscard = function (array) {
-  for (let i = 0; i < array.length; i++) {
-    switch (array[i]) {
-      case '--discard-next':
-        if (array[i + 1] === undefined) { array.splice(i, 1); i -= 1; } else { array.splice(i, 2); i -= 1; }
+        doublePrev(i);
         break;
       case '--discard-prev':
-        if (array[i - 1] === undefined) { array.splice(i, 1); i -= 1; } else { array.splice(i - 1, 2); i -= 2; }
+        discardPrev();
         break;
+      default:
+        resultArr.push(arr[i]);
     }
   }
+
+  return resultArr;
 };
